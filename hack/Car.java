aacdac768b4ad8ffa05051552d2fd871db2c0aed
REALDrummer <conman.davey@gmail.com>
2015-03-07 06:10:12 -0700

    Added painting!
    Signed-off-by: REALDrummer <conman.davey@gmail.com>

--- hack/Car.java
+++ hack/Car.java
@@ -1,33 +1,44 @@
 package hack;
 
-import java.awt.*;
+import hack.GridLock.Paintable;
 
-public class Car {
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Car implements Paintable {
+    public static final ArrayList<Car> CARS = new ArrayList<>();
 
     private float acceleration;
     private Point location;
     private double velocity;
     private boolean halfway;
 
-    Car(float acceleration, Point location, double velocity){
+    Car(float acceleration, Point location, double velocity) {
         this.acceleration = acceleration;
         this.location = location;
         this.velocity = velocity;
     }
 
-    float getAcceleration(){
+    float getAcceleration() {
         return acceleration;
     }
 
-    Point getLocation(){
+    Point getLocation() {
         return location;
     }
 
-    double velocity(){
+    double velocity() {
         return velocity;
     }
 
-    boolean isHalfWay(){
+    boolean isHalfWay() {
         return halfway;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        // represent cars using yellow circles as wide as the lanes
+        g.setColor(Color.YELLOW);
+        g.fillOval(location.x - Road.LANE_WIDTH / 2, location.y - Road.LANE_WIDTH / 2, Road.LANE_WIDTH, Road.LANE_WIDTH);
     }
 }
--- hack/GridLock.java
+++ hack/GridLock.java
@@ -2,6 +2,7 @@ package hack;
 
 import hack.Intersection.IntersectionType;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -12,14 +13,20 @@ import javax.swing.JApplet;
 public class GridLock extends JApplet {
     private static final long serialVersionUID = 1099492150132430698L;
 
-    public static int GRID_WIDTH = 3, GRID_HEIGHT = 2;
+    public static final int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 800;
+    public static final int GRID_WIDTH = 3, GRID_HEIGHT = 2;
 
     public static GridLock applet;
     public static Container content;
     public static Graphics2D graphics;
+
     public static Point mouse_location, last_mouse_location;
 
     public static Intersection currently_viewed_intersection = null;
+
+    public static interface Paintable {
+        void paint(Graphics g);
+    }
 
     @Override
     public void init() {
@@ -34,10 +41,16 @@ public class GridLock extends JApplet {
             new Intersection(IntersectionType.STOP_LIGHT);
 
         // create the roads
-        for (int i = 0; i < (GRID_WIDTH * 2 + 1) * (GRID_HEIGHT * 2 + 1); i++)
-            new Road(1, 0, 0, 0, 0);
+        for (int i = 0; i < (GRID_WIDTH + 1) * (GRID_HEIGHT * 2 + 1) - (GRID_HEIGHT + 1); i++)
+            new Road(0, 1, 0, 0, 0);
 
-        setSize(800, 800);
+        // calculate the width of a lane based on the number of roads and the size of the screen
+        /* the scaling below is based on a scaling model in which each road when assumed to be four lanes should have a width equal to 1/3 the length of road between two
+         * intersectins; use this to calculate the lane width of both NS and EW roads, then use the smaller of the two values as the global lane width */
+        int NS_lane_width = WINDOW_WIDTH / (GRID_WIDTH * 4 + 3) / 4, EW_lane_width = WINDOW_HEIGHT / (GRID_HEIGHT * 4 + 3);
+        Road.LANE_WIDTH = NS_lane_width < EW_lane_width ? NS_lane_width : EW_lane_width;
+
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
         mouse_location = applet.getMousePosition();
         last_mouse_location = null;
@@ -46,6 +59,23 @@ public class GridLock extends JApplet {
 
     @Override
     public void paint(Graphics g) {
-        // TODO
+        // make the green background
+        g.setColor(Color.GREEN);
+        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        // paint the roads
+        for (Road[] road_array : Road.ROADS)
+            for (Road road : road_array)
+                if (road != null)
+                    road.paint(g);
+
+        // paint the intersections
+        for (int i = 0; i < Intersection.INTERSECTIONS.length; i++)
+            for (int j = 0; j < Intersection.INTERSECTIONS[i].length; j++)
+                Intersection.INTERSECTIONS[i][j].paint(g);
+
+        // paint the cars
+        for (Car car : Car.CARS)
+            car.paint(g);
     }
 }
