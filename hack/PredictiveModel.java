20df033af8eeb4359494b8b43226d8beed2e0072
johncava <john.kevin.cava@gmail.com>
2015-03-07 03:44:44 -0800

    setting up predictive model

---
+++ hack/PredictiveModel.java
@@ -0,0 +1,15 @@
+package hack;
+
+
+public class PredictiveModel {
+    private static final int SIZE = GridLock.GRID_WIDTH + GridLock.GRID_HEIGHT;
+    private static final float congestionMatrix[] = new float[SIZE];
+    private static final float adjacencyMatrix[][] = new float[GridLock.GRID_WIDTH][GridLock.GRID_HEIGHT];
+
+    PredictiveModel(){
+        /*
+            Need to Initialize the Predictive Model Later
+         */
+    }
+
+}
